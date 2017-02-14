691de8da8202630d4a636d44fa67dedbebafcd89
AAKASH R <munna_akash@yahoo.co.in>
2017-02-14 21:36:41 +0530

    Update TLS.m

--- Assignment2/TLS.m
+++ Assignment2/TLS.m
@@ -11,7 +11,7 @@ reg_1 = (-reg_1/reg_1(end));
 reg_1 = reg_1(1:(end-1));
 %% 
 d_1 = D_1 + (-D_1*V_1(:,end))*(V_1(:,end)') ; % Transforming Data to new basis
-X_1 = d_1(:,1:(n-1));
+X_1 = D_1(:,1:(n-1));
 e1 = (X_1((m1+1):end,:)*reg_1-D_1((m1+1):end,n))*std1;
 RMSE_TLS_1 = rms((X_1((m1+1):end,:)*reg_1-D_1((m1+1):end,n))*std1); % Scale of 10
 
